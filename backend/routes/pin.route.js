9f3d1d9af6b8fb8e19237589a573cdf89de65f34
luyenkieu22 <twonine569@gmail.com>
2025-05-31 23:06:10 +0700

    add api user, comment, board, verifyToken

---
+++ backend/routes/pin.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+    getPins,
+    getPin,
+    createPin,
+} from "../controllers/pin.controller.js";
+import { verifyToken } from "../middlewares/verifyToken.js";
+
+const router = express.Router();
+
+router.get("/", getPins);
+router.get("/:id", getPin);
+router.post("/", verifyToken, createPin);
+
+export default router;
---
+++ backend/routes/user.route.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+    getUser,
+    registerUser,
+    loginUser,
+    logoutUser,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/:username", getUser);
+router.post("/auth/register", registerUser);
+router.post("/auth/login", loginUser);
+router.post("/auth/logout", logoutUser);
+
+export default router;
